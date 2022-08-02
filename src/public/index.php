d862a4fdbb0f5e6ab87f6ecd1878c6691fe9aceb
elespejodetarkovsky <sebastian.turone@gmail.com>
2022-08-02 18:22:22 +0200

    index solo

--- src/public/index.php
+++ src/public/index.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Kernel;
-
-print "hola";
+//use App\Kernel;
+//
+//echo "hola";
 //require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 //
 //return function (array $context) {
 //    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+//};
+phpinfo();
+?>
